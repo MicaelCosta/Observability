0f53b56a85d0d7c97a719e7b66f21a737c155391
Micael <micael@autoware.com.br>
2023-06-13 07:15:01 -0300

    feat: exclude front

--- infra/init.bat
+++ infra/init.bat
@@ -1,6 +1,3 @@
-cd C:\Program Files\RabbitMQ Server\rabbitmq_server-3.11.16\sbin
-start rabbitmq-server.bat
-
 start C:\repositories\observability\infra\jaeger-all-in-one.exe
 
 cd C:\repositories\observability\infra\redis
